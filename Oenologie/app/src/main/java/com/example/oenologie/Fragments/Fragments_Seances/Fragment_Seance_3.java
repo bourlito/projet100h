46cee47b51ee620a240b853bc8aa5ac7eac92d3d
Etienne Le Bourlout <etienne.le-bourlout@hei.yncrea.Fr>
2018-02-09 15:25:15 +0100

    premier parse

--- Oenologie/app/src/main/java/com/example/oenologie/Fragments/Fragments_Seances/Fragment_Seance_3.java
+++ Oenologie/app/src/main/java/com/example/oenologie/Fragments/Fragments_Seances/Fragment_Seance_3.java
@@ -9,13 +9,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.oenologie.ConnexionTomcat.AsyncResponse;
+import com.example.oenologie.ConnexionTomcat.RecupererJson;
 import com.example.oenologie.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Fragment_Seance_3 extends Fragment {
+public class Fragment_Seance_3 extends Fragment implements AsyncResponse{
     private TextView tvfrags3;
+    private TextView tvfrags32;
+    private TextView tvfrags33;
+    private String url = "http://10.34.170.211/php/seance1";
 
     public Fragment_Seance_3() {
         // Required empty public constructor
@@ -33,7 +42,20 @@ public class Fragment_Seance_3 extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         tvfrags3 = view.findViewById(R.id.tvfrags3);
-
-        tvfrags3.setText("Déroulement :...\n\n\nVins à découvir :...");
+        tvfrags32 = view.findViewById(R.id.tvfrags32);
+        tvfrags33 = view.findViewById(R.id.tvfrags33);
+        RecupererJson recupererJson = new RecupererJson();
+        recupererJson.delegate = this;
+        recupererJson.execute(url);
     }
+
+
+    @Override
+    public void processFinish(String output) throws JSONException {
+        JSONObject mainObject = new JSONObject(output);
+        JSONArray mainArray = mainObject.getJSONArray("server_response");
+        JSONObject unicObject = mainArray.getJSONObject(0);
+        tvfrags3.setText(unicObject.getString("Date"));
+        tvfrags32.setText(unicObject.getString("libelle"));
+        tvfrags33.setText(unicObject.getString("Infos"));    }
 }
