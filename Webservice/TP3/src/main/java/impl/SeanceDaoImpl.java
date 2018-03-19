8554923748159f999dece08e894730e3098de72f
Chevalier Thomas <thomas.chevalier@hei.yncrea.fr>
2018-03-19 10:03:42 +0100

    ok

--- Webservice/TP3/src/main/java/impl/SeanceDaoImpl.java
+++ Webservice/TP3/src/main/java/impl/SeanceDaoImpl.java
@@ -8,8 +8,36 @@ import java.sql.*;
 public class SeanceDaoImpl implements SeanceDao {
 
     @Override
-    public Seance getSeance(Integer Id) {
+    public Seance getSeance1 (Integer Id) {
         String query = "SELECT * FROM `seance` WHERE Id_seance=1";
+        try(
+                Connection connection = DatasourceProvider.getDaraSource().getConnection();
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(query)
+        ) {
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public Seance getSeance2 (Integer Id) {
+        String query = "SELECT * FROM `seance` WHERE Id_seance=2";
+        try(
+                Connection connection = DatasourceProvider.getDaraSource().getConnection();
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(query)
+        ) {
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public Seance getSeance3 (Integer Id) {
+        String query = "SELECT * FROM `seance` WHERE Id_seance=3";
         try(
                 Connection connection = DatasourceProvider.getDaraSource().getConnection();
                 Statement statement = connection.createStatement();
