d2f80f609eca4ae376e96d8217089330f567035d
Chevalier Thomas <thomas.chevalier@hei.yncrea.fr>
2018-03-19 10:02:01 +0100

    ok

--- Webservice/TP3/src/main/java/impl/SeanceDao.java
+++ Webservice/TP3/src/main/java/impl/SeanceDao.java
@@ -3,6 +3,8 @@ package impl;
 import entities.Seance;
 
 public interface SeanceDao {
-    public Seance getSeance ( Integer Id);
+    public Seance getSeance1 ( Integer Id);
+    public Seance getSeance2 ( Integer Id);
+    public Seance getSeance3 ( Integer Id);
     public Seance updateSeance (Integer id, String libelle, String date, String informations);
 }
